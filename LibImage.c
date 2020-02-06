81a6092e839f2306e29d4fae0a67280c37941b95
segevtomer <42846571+segevtomer@users.noreply.github.com>
2020-02-06 23:14:27 +0200

    libimage with doc

---
+++ LibImage.c
@@ -0,0 +1,624 @@
+#include "libimage.h"
+
+
+void readFromBuffer(png_structp png_ptr, png_bytep dataBuffer, png_size_t bytesToRead)
+{
+    png_voidp io_ptr = png_get_io_ptr(png_ptr);
+    if (!io_ptr)
+    {
+        printf("failed to fetch png_io_ptr\n");
+        return;
+    }
+
+    Buffer* inBuffer = (Buffer*)io_ptr;
+    memcpy(dataBuffer, inBuffer->buf, bytesToRead);
+    inBuffer->buf += bytesToRead;
+}
+
+void writeToBuffer(png_structp png_ptr, png_bytep dataBuffer, png_size_t bytesToRead)
+{
+    png_voidp io_ptr = png_get_io_ptr(png_ptr);
+    if (!io_ptr)
+    {
+        printf("failed to fetch png_io_ptr\n");
+        return;
+    }
+
+    Buffer* outBuffer = (Buffer*)io_ptr;
+
+    if (!outBuffer->buf)
+    {
+        outBuffer->buf = (byte*)malloc(sizeof(byte) * bytesToRead);
+        if (!outBuffer->buf)
+        {
+            printf("allocation failed for out-buffer\n");
+            return;
+        }
+    }
+    else
+    {
+        byte* newBuf = (byte*)realloc(outBuffer->buf, sizeof(byte) * (outBuffer->size + bytesToRead));
+        if (!newBuf)
+        {
+            printf("realloc failed for out-buffer\n");
+            free(outBuffer->buf);
+            return;
+        }
+        outBuffer->buf = newBuf;
+    }
+    memcpy(outBuffer->buf + outBuffer->size, dataBuffer, bytesToRead);
+    outBuffer->size += bytesToRead;
+}
+
+/*
+* return suffices here because once you return prematurely from read/write procedures libpng will
+* detect the missing bytes in a short while (be it a null ptr, missing magic numbers or bad crc)
+* once an error is raised, the next block of execution will be the setjmp that's in open/save
+*/
+
+bool isFormatMatch(const byte* inBuffer, const byte* format, int formatLen)
+{
+    int i;
+    for (i = 0; i < formatLen; ++i)
+    {
+        if (inBuffer[i] == '\0' || inBuffer[i] != format[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool formatCompIgnoreCase(const char* s1, const char* s2)
+{
+    int size1 = strlen(s1);
+    if (strlen(s2) != size1)
+    {
+        return false;
+    }
+    int i;
+    for (i = 0; i < size1; ++i)
+    {
+        if (toupper(s1[i]) != s2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+enum format isFormatSupported(const byte* inBuffer)
+{
+    if (!inBuffer)
+    {
+        return NoneFormat;
+    }
+
+    if (isFormatMatch(inBuffer, jpeg, JPEG_L) || isFormatMatch(inBuffer, jpeg2, JPEG_L))
+    {
+        return Jpeg;
+    }
+    if (isFormatMatch(inBuffer, png, PNG_L))
+    {
+        return Png;
+    }
+    return NoneFormat;
+}
+
+enum colorType pngColorTypeDictionary(byte colorType)
+{
+    switch (colorType)
+    {
+    case 0:
+        return GrayScale;
+    case 2:
+        return RGB;
+    case 3:
+        return PLTE;
+    case 4:
+        return GSA;
+    case 6:
+        return RGBA;
+    default:
+        return NoneType;
+    }
+}
+
+bool openImage(const byte* inBuffer, Image** image)
+{
+    enum format imageFormat = isFormatSupported(inBuffer);
+    switch (imageFormat)
+    {
+    case Png:
+        return handleOpenPng(inBuffer, image);
+    case Jpeg:
+        /*
+         * return handleOpenJpeg(buf, image);
+         * support for the format can be added easily in the future
+         */
+    default:
+        return false;
+    }
+}
+
+bool handleOpenPng(const byte* inBuffer, Image** image)
+{
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr)
+    {
+        printf("creation of png_structp failed\n");
+        return false;
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+    {
+        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+        printf("creation of png_infop failed\n");
+        return false;
+    }
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        printf("error while reading header\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+        return false;
+    }
+
+    Buffer pngReadBuffer = { inBuffer, 0 };
+    png_set_read_fn(png_ptr, &pngReadBuffer, readFromBuffer); 
+    png_set_sig_bytes(png_ptr, 0);
+
+    png_read_info(png_ptr, info_ptr);
+
+    size_t width, height;
+    byte colorType, bitDepth;
+    byte** rowPtrs;
+
+    width = png_get_image_width(png_ptr, info_ptr);
+    height = png_get_image_height(png_ptr, info_ptr);
+    colorType = png_get_color_type(png_ptr, info_ptr);
+    bitDepth = png_get_bit_depth(png_ptr, info_ptr);
+
+    enum colorType colorTypeEnum = pngColorTypeDictionary(colorType);
+    if (colorTypeEnum == NoneType)
+    {
+        printf("data integrity error, color-type is unknown\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+        return false;
+    }
+
+    png_read_update_info(png_ptr, info_ptr);
+
+    rowPtrs = (byte**)malloc(sizeof(byte*) * height);
+    if (!rowPtrs) 
+    {
+        printf("allocation for binary image data failed\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+        return false;
+    }
+    size_t i;
+    for (i = 0; i < height; ++i)
+    {
+        rowPtrs[i] = (byte*)malloc(png_get_rowbytes(png_ptr, info_ptr));
+        if (!rowPtrs[i])
+        {
+            printf("allocation for binary image data failed\n");
+            freeRows(rowPtrs, i);
+            png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+            return false;
+        }
+    }
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        printf("failed reading the image\n");
+        freeRows(rowPtrs,height);
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+        return false;
+    }
+
+    png_read_image(png_ptr, rowPtrs);
+
+    png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+
+    Image* img = (Image*)malloc(sizeof(Image));
+    if (!img)
+    {
+        printf("image allocation failed\n");
+        freeRows(rowPtrs, height);
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+        return false;
+    }
+    img->height = height;
+    img->width = width;
+    img->bitDepth = bitDepth;
+    img->colorTypeVal = colorType;
+    img->colorTypeEnum = colorTypeEnum;
+    img->rowPtrs = rowPtrs;
+
+    *image = img;
+
+    return true;
+}
+
+void freeRows(byte** rowPtrs, size_t height) 
+{
+    size_t i;
+    for (i = 0; i < height; ++i)
+    {
+        free(rowPtrs[i]);
+    }
+    free(rowPtrs);
+}
+
+bool saveImage(Image* image, const char* format, byte** outBuffer)
+{
+    if (!image || !format)
+    {
+        return false;
+    }
+
+    if (formatCompIgnoreCase(format, PNG))
+    {
+        return handleSavePng(image, outBuffer);
+    }
+    if (formatCompIgnoreCase(format, JPEG))
+    {
+        /*
+         * return handleSaveJpeg(image, retBuffer);
+         * support for the format can be added easily in the future
+         */
+    }
+
+    return false;
+}
+
+bool handleSavePng(Image* image, byte** outBuffer)
+{
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr)
+    {
+        printf("creation of png_structp failed\n");
+        return false;
+    }
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+    {
+        printf("creation of png_infop failed\n");
+        png_destroy_write_struct(&png_ptr, NULL);
+        return false;
+    }
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        printf("failed setting header info\n");
+        png_destroy_write_struct(&png_ptr, &info_ptr);
+        return false;
+    }
+    png_set_IHDR(png_ptr, info_ptr, image->width, image->height, image->bitDepth, image->colorTypeVal,
+        PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+    png_set_compression_level(png_ptr, 1);
+    png_set_filter(png_ptr, 0, PNG_ALL_FILTERS);
+    png_set_rows(png_ptr, info_ptr, image->rowPtrs);
+
+    byte* emptyBuffer = NULL;
+    Buffer pngWriteBuffer = { emptyBuffer, 0 };
+    png_set_write_fn(png_ptr, &pngWriteBuffer, writeToBuffer, NULL);
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        printf("failed writing image data\n");
+        if (!pngWriteBuffer.buf)
+        {
+            free(outBuffer);
+        }
+        png_destroy_write_struct(&png_ptr, &info_ptr);
+        return false;
+    }
+
+    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    freeRows(image->rowPtrs, image->height);
+    free(image);    
+    png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+
+    *outBuffer = pngWriteBuffer.buf;
+    return true;
+}
+
+bool averageImage(int avgDim, Image* image)
+{
+    if (image->colorTypeEnum == RGBA && image->bitDepth == 8)
+    {
+        return handleEightBitRgbaAveraging(image, avgDim);
+    }
+    return false;
+}
+
+bool handleEightBitRgbaAveraging(Image* image, int avgDim)
+{
+    size_t newHeight = image->height / avgDim;
+    size_t newWidth = image->width / avgDim;
+    int travDistance = avgDim - 1;
+    byte** rows = image->rowPtrs;
+
+    byte** newRows = createAvgImage(rows, newHeight, newWidth, avgDim);
+    if (!newRows)
+    {
+        return false;
+    }
+
+    image->height = newHeight;
+    image->width = newWidth;
+    image->rowPtrs = newRows;
+
+    return true;
+}
+
+bool paveImage(int numOfImgs, Image* image, Image*** imageChunks)
+{   
+    if (image->colorTypeEnum == RGBA && image->bitDepth == 8)
+    {
+        return handleEightBitRgbaPaving(image, numOfImgs, imageChunks);
+    }
+    return false;
+}
+
+bool handleEightBitRgbaPaving(Image* image, int numOfImgs, Image*** imageChunks)
+{
+    size_t newHeight = image->height / numOfImgs;
+    size_t newWidth = image->width / numOfImgs;
+    int travDistance = numOfImgs - 1;
+    byte** rows = image->rowPtrs;
+
+    int size = numOfImgs * numOfImgs;
+    byte*** newRowsList = (byte***)malloc(sizeof(byte**) * size);
+    if (!newRowsList)
+    {
+        printf("allocation for chunked image list failed\n");
+        return false;
+    }
+
+    int outerX, outerY;
+    size_t y, x;
+    for (outerY = 0; outerY < numOfImgs; ++outerY)
+    {
+        for (outerX = 0; outerX < numOfImgs; ++outerX)
+        {
+            int chunkOffest = outerY * numOfImgs + outerX;
+            byte** newRows = (byte**)malloc(sizeof(byte*) * newHeight);
+            if (!newRows)
+            {
+                printf("allocation for chunked image list failed\n");
+                int i;
+                for (i = 0; i < chunkOffest; ++i)
+                {
+                    freeRows(newRowsList[i], newHeight);
+                }
+                return false;
+            }
+
+            newRowsList[chunkOffest] = newRows;
+            size_t y_offest = outerY * newHeight;
+            size_t x_offest = outerX * newWidth * 4;
+            for (y = 0; y < newHeight; ++y)
+            {
+                byte* row = rows[y + y_offest];
+                byte* newRow = (byte*)malloc(sizeof(byte) * newWidth * 4);
+                if (!newRow)
+                {
+                    printf("allocation for chunked image list failed\n");
+                    freeRows(newRows, y);
+                    int i;
+                    for (i = 0; i < chunkOffest - 1; ++i)
+                    {
+                        freeRows(newRowsList[i], newHeight);
+                    }
+                    return false;
+                }
+
+                newRows[y] = newRow;
+                for (x = 0; x < newWidth; ++x)
+                {
+                    unsigned int pos = x * 4;
+                    newRow[pos] = row[pos + x_offest];
+                    newRow[pos + 1] = row[pos + x_offest + 1];
+                    newRow[pos + 2] = row[pos + x_offest + 2];
+                    newRow[pos + 3] = row[pos + x_offest + 3];
+                }
+            }
+        }
+    }
+
+    Image** images = (Image**)malloc(sizeof(Image*) * size);
+
+    for (x = 0; x < size; ++x)
+    {
+        images[x] = (Image*)malloc(sizeof(Image));
+        images[x]->height = newHeight;
+        images[x]->width = newWidth;
+        images[x]->bitDepth = image->bitDepth;
+        images[x]->colorTypeVal = image->colorTypeVal;
+        images[x]->colorTypeEnum = image->colorTypeEnum;
+        images[x]->rowPtrs = newRowsList[x];
+    }
+
+    *imageChunks = images;
+
+    return true;
+}
+
+bool splitImage(int numOfImgs, Image* image)
+{
+    size_t newHeight = image->height / numOfImgs;
+    size_t newWidth = image->width / numOfImgs;
+    int travDistance = numOfImgs - 1;
+    byte** rows = image->rowPtrs;
+
+    byte** newRows = createAvgImage(rows, newHeight, newWidth, numOfImgs);
+    if (!newRows) 
+    {
+        return false;
+    }
+
+    int outerY, outerX;
+    size_t y, x;
+    for (outerY = 0; outerY < numOfImgs; ++outerY)
+    {
+        for (outerX = 0; outerX < numOfImgs; ++outerX)
+        {
+            size_t y_offest = outerY * newHeight;
+            size_t x_offest = outerX * newWidth * 4;
+            for (y = 0; y < newHeight; ++y)
+            {
+                byte* row = rows[y + y_offest];
+                byte* newRow = newRows[y];
+                for (x = 0; x < newWidth; ++x) 
+                {
+                    unsigned int pos = x * 4;
+                    row[pos + x_offest] = newRow[pos];
+                    row[pos + x_offest + 1] = newRow[pos + 1];
+                    row[pos + x_offest + 2] = newRow[pos + 2];
+                    row[pos + x_offest + 3] = newRow[pos + 3];
+                }
+            }
+        }
+    }
+    return true;
+}
+
+byte** createAvgImage(byte** rows, size_t newHeight, size_t newWidth, int numOfImgs)
+{
+    byte** newRows = (byte**)malloc(sizeof(byte*) * newHeight);
+    if (!newRows)
+    {
+        printf("failed to allocate memory for averaged image");
+        return NULL;
+    }
+
+    size_t y, x;
+    for (y = 0; y < newHeight; ++y)
+    {
+        byte* row = rows[y];
+        byte* newRow = (byte*)malloc(sizeof(byte) * newWidth * 4);
+        if (!newRow)
+        {
+            printf("failed to allocate memory for averaged image");
+            freeRows(newRows, y);
+            return NULL;
+        }
+        newRows[y] = newRow;
+        for (x = 0; x < newWidth; ++x)
+        {
+            size_t pos = x * 4;
+            int avgs[4] = { 0 };
+            calcAverage(rows, numOfImgs, pos * numOfImgs, y * numOfImgs, avgs);
+            newRow[pos++] = avgs[0];
+            newRow[pos++] = avgs[1];
+            newRow[pos++] = avgs[2];
+            newRow[pos] = avgs[3];
+        }
+    }
+    return newRows;
+}
+
+void calcAverage(byte** rows, int avgDim, size_t start_x, size_t start_y, int* avgs)
+{
+    int y = 0, x = 0;
+    int sum[4] = { 0 };
+    for (y = 0; y < avgDim; ++y)
+    {
+        byte* row = rows[y + start_y];
+        for (x = 0; x < avgDim * 4; x += 4)
+        {
+            size_t pos = x + start_x;
+            sum[0] += row[pos++];
+            sum[1] += row[pos++];
+            sum[2] += row[pos++];
+            sum[3] += row[pos];
+        }
+    }
+    int size = avgDim * avgDim;
+    for (x = 0; x < 4; ++x)
+    {
+        avgs[x] = sum[x] / size;
+    }
+}
+
+void write_png_file(Image* image, char* file_name)
+{
+    FILE* fp = fopen(file_name, "wb");
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (setjmp(png_jmpbuf(png_ptr)))
+        return;
+    png_init_io(png_ptr, fp);
+    png_set_IHDR(png_ptr, info_ptr, image->width, image->height,
+        image->bitDepth, image->colorTypeVal, PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_write_info(png_ptr, info_ptr);
+    png_write_image(png_ptr, image->rowPtrs);
+    png_write_end(png_ptr, NULL);
+    fclose(fp);
+}
+
+int main()
+{
+    FILE* fp;
+    byte* buffer, *buf2, *buf3, *buf4;
+    long flen;
+
+    fp = fopen("image2.png", "rb");
+    fseek(fp, 0, SEEK_END);
+    flen = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    buffer = (byte*)malloc((flen) * sizeof(byte));
+    size_t bytes_read = fread(buffer, sizeof(byte), flen, fp);
+    fclose(fp);
+    buf2 = buf3 = buf4 =buffer;
+
+    Image* image = NULL;
+    if (openImage(buf2, &image))
+    {
+        printf("success!!!!!!\n");
+    }
+    byte* buf = NULL;
+
+    write_png_file(image, "test1.png");
+
+    if (saveImage(image, "pNg", &buf))
+    {
+        printf("double success!!\n");
+    }
+
+    Image* image2 = NULL;
+    if (openImage(buf, &image2))
+        printf("success!!!!!!\n");
+    printf("average this mfer!\n");
+    averageImage(4, image2);
+
+    write_png_file(image2, "test2.png");
+    
+    saveImage(image2,"png", &buf3);
+
+    Image* image3 = NULL;
+    if (openImage(buf3, &image3))
+    {
+        printf("get ready for zeruf\n");
+    }
+    Image** images;
+    int size = 4;
+    paveImage(size, image3, &images);
+    int i;
+    for (i = 0; i < size*size; ++i)
+    {
+        char buf[10];
+        sprintf(buf, "tst%d.png", i);
+        write_png_file(images[i], buf);
+    }
+    
+    return 0;
+}
---
+++ libimage.h
@@ -0,0 +1,187 @@
+#pragma once
+#define _CRT_SECURE_NO_DEPRECATE
+#pragma warning (disable : 4996)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <png.h>
+
+#define JPEG "JPEG"
+#define PNG "PNG"
+
+#define JPEG_L 4
+#define PNG_L 8
+
+/*
+* the library's external API (open,save,avg,pave) all return a boolean
+* the boolean value indicates whether the call is successful or not
+* in the event of success, the functions allocate the request to the last
+* input parameter (e.g: openImage allocates and sets the values for argument 'image')
+*
+*
+* general note: whenever multiple loop arguments are called back-to-back instead of using
+* an inner loop (which to be fair could look a bit nicer), it is done to take advantage of
+* the performance boost of loop-unrolling and the locality principle on the referenced data
+*
+*
+* DISCLAIMER:
+* current implementation, due to time constraints, supports only 8 bit-depth RGBA
+* PNG images (for average and pave manipulations). support for other variations is
+* expandable and the only detriment for it under development was time limitations.
+*/
+
+enum format
+{
+    NoneFormat, Png, Jpeg, Bmp, Gif
+};
+
+enum colorType
+{
+    NoneType, GrayScale,GSA,RGB,RGBA,PLTE
+};
+
+typedef unsigned char byte;
+
+/*
+ * format magic numbers
+ */
+const byte png[8] = { 137, 80, 78, 71, 13, 10, 26, 10 };
+const byte jpeg[4] = { 255, 216, 255, 224 };
+const byte jpeg2[4] = { 255, 216, 255, 225 };
+
+/*
+ * more formats, may be added in the future
+ */
+const byte bmp[2] = { 42, 40 };
+const byte gif[6] = { 47, 49, 46, 38, 39, 61 };
+const byte gif2[6] = { 47, 49, 46, 38, 37, 61 };
+
+/*
+* the Image struct is polymorphic for any image type, each image-type-handler
+* will allocate the data from the specific format type to the Image struct
+*/
+typedef struct
+{
+    byte** rowPtrs;
+    size_t width;
+    size_t height;
+    byte bitDepth;
+    byte colorTypeVal;
+    enum colorType colorTypeEnum;
+} Image;
+
+
+/*
+* the Buffer struct is used for read&write capabilities, in order to open the image
+* from an in-memory buffer (at least in the specific case of the png-handler), the
+* libpng library required me to provide my own I/O procedures - hence Buffer was made
+*/
+typedef struct
+{
+    byte* buf;
+    size_t size;
+} Buffer;
+
+
+/*
+* receives a byte array and a NULL image ptr, verifies the format
+* is supported and redirects it to the relevant format-open-handler
+* ret val is indicaction of success, in case of success 'image' will
+* be allocated with the image data
+*/
+bool openImage(const byte* buf, Image** image);
+
+/*
+* receives an image ptr, format string and NULL ptr of byte array, verifies the save
+* format is supported and redirects it to the relevant format-save-handler
+* ret val is indicaction of success, in case of success retBuffer will reference the
+* address of the byte array that is the binary representation of the Image argument
+*/
+bool saveImage(Image* image, const char* format, byte** retBuffer);
+
+/*
+* receives an image ptr and the requested dimension to be used for the average calculation
+* in case of allocation failures the state of the image ptr is unaltered and ret val is false
+* in case of success the image ptr is altered with the newly calculated average data
+*/
+bool averageImage(int avgDim, Image* image);
+
+/*
+* receives an image ptr, the requested num of images to split the image in-to
+* and the ptr to the (not-yet-allocated and-) soon to be array of chunked images
+* the number of images determines how many chunks are to be made (numOfImages^2)
+* in case of success, imageChunks will now point to an array of allocated image
+* ptrs where each image ptr holds the data of a chunk of the original image
+*/
+bool paveImage(int numOfImgs, Image* image, Image*** imageChunks);
+
+
+/*
+* specific handlers for parsing the matrix of image data with 8-bit depth and RGBA channels
+* an expandle solution, new handlers will be able to support more bit-depth and color-types
+*/
+bool handleEightBitRgbaPaving(Image* image, int numOfImgs, Image*** imageChunks);
+bool handleEightBitRgbaAveraging(Image* image, int avgDim);
+
+/*
+* helper function for calculating the average value for the given dimension.
+* rows is the original matrix, avgDim dictates the size of the average matrix, 
+* start_x and start_y are coordinates for the origin point to be used in rows
+* edge case handling is done outside calcAverage prior to its call in averageImage
+* in the scenario of success the averages are returned using the last argument
+*/
+void calcAverage(byte** rows, int avgDim, size_t start_x, size_t start_y, int* avgs);
+
+/*
+* creates and allocates the memory for the averaged binary matrix that represents the averaged image
+* in case of success, the matrix is to be returned. in case of failure, return value is NULL
+*/
+byte** createAvgImage(byte** rows, size_t newHeight, size_t newWidth, int numOfImgs);
+
+
+/*
+* this following 2 procedures override the default I/O procedures for the libpng library
+* this is done to allow libpng's API to use my provided buffers instead of the default file stream
+*/
+void readFromBuffer(png_structp png_ptr, png_bytep dataBuffer, png_size_t bytesToRead);
+void writeToBuffer(png_structp png_ptr, png_bytep dataBuffer, png_size_t bytesToRead);
+
+
+/*
+* specific type handlers that are called from their generic external counterparts
+* in the case of addition of future formats, each format will receive its own handler
+*/
+bool handleOpenPng(const byte* buf, Image** image);
+bool handleOpenJpeg(const byte* buf, Image** image);
+bool handleSavePng(Image* image, byte** buf);
+bool handleSaveJpeg(Image* image, byte** buf);
+
+/*
+* helper function for freeing allocated image data
+*/
+void freeRows(byte** rowPtrs, size_t height);
+
+
+/*
+* compares the binary data with predefined constant format magic numbers
+* was added as a help function because it deals easily with edge cases
+* each format may have a different length of magic number bits and using
+* this function saves the need from validating the buffer's length
+*/
+bool isFormatMatch(const byte* buf, const byte* format, int formatLen);
+
+/*
+* returns an enum representation of the buffer's format
+* enum value 'None' is returned if it is not supported
+*/
+enum format isFormatSupported(const byte* buf);
+
+/*
+* verifies the requested save format in saveImage is supported (case-insensitive to *INPUT*)
+* takes the assumption that the constant of the format is UPPERCASED, so each newly added
+* format will need to follow this convention (define uppercased constant)
+*/
+bool formatCompIgnoreCase(const char* s1, const char* s2);
